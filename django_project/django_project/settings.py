735f5db605de4a457c57a6cf41e38088ea76cb96
sonnygy <166960915+sonnygy@users.noreply.github.com>
2025-06-08 05:18:27 +0300

    settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -70,16 +70,17 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb',
-        'USER': 'user',
-        'PASSWORD': 'password',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': os.environ.get('POSTGRES_DB', 'mydb'),
+        'USER': os.environ.get('POSTGRES_USER', 'user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
         'OPTIONS': {
             'options': '-c search_path=django_project_schema,public'
         }
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
